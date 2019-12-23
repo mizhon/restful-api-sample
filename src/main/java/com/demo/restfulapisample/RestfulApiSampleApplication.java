5f7429602463bf06bb593a80b25fea9f9bd051fc
mizhon <mizhon@live.cn>
2019-12-23 13:49:32 +0800

    git init

---
+++ src/main/java/com/demo/restfulapisample/RestfulApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.restfulapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiSampleApplication.class, args);
+	}
+
+}
