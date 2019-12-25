6cc13276c12db0194b7cb999679715e7501769e4
mizhon <mizhon@live.cn>
2019-12-25 18:02:10 +0800

    添加支持跨域配置

--- src/main/java/com/demo/restfulapisample/conf/WebMvcConfigAdapter.java
+++ src/main/java/com/demo/restfulapisample/conf/WebMvcConfigAdapter.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author ming
+ */
 @Configuration
 public class WebMvcConfigAdapter implements WebMvcConfigurer {
 
